0d6600fc7f4a99df769f31c767ff2ae128b61b91
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-20 19:30:20 -0400

    added client methods, samplserv routes, and one hot remix

--- lib/client.rb
+++ lib/client.rb
@@ -9,10 +9,148 @@ class SamplParty
     SamplParty.post("/better", :body => options)
   end
 
+  def after(options={})
+    SamplParty.post("/after", :body => options)
+  end
+
+  def doit(options={})
+    SamplParty.post("/doit", :body => options)
+  end
+
+  def ever(options={})
+    SamplParty.post("/ever", :body => options)
+  end
+
+  def faster(options={})
+    SamplParty.post("/faster", :body => options)
+  end
+
+  def harder(options={})
+    SamplParty.post("/harder", :body => options)
+  end
+
+  def hour(options={})
+    SamplParty.post("/hour", :body => options)
+  end
+
+  def makeit(options={})
+    SamplParty.post("/makeit", :body => options)
+  end
+
+ def makesus(options={})
+    SamplParty.post("/makesus", :body => options)
+  end
+
+  def morethan(options={})
+    SamplParty.post("/morethan", :body => options)
+  end
+
+   def never(options={})
+    SamplParty.post("/never", :body => options)
+  end
+
+   def our(options={})
+    SamplParty.post("/our", :body => options)
+  end
+
+   def over(options={})
+    SamplParty.post("/over", :body => options)
+  end
+
+   def stronger(options={})
+    SamplParty.post("/stronger", :body => options)
+  end
+
+   def workis(options={})
+    SamplParty.post("/workis", :body => options)
+  end
+
+   def workit(options={})
+    SamplParty.post("/workit", :body => options)
+  end
+
+  def beat
+    SamplParty.post("/beat")
+  end
+
   def stop
     SamplParty.get("/stop")
   end
 end
 
-binding.pry
+
+party = SamplParty.new
+
+party.beat
+sleep 1
+party.workit(v: 1)
+sleep 2
+party.better(v: 1)
+sleep 1
+party.never(v: 2)
+sleep 2
+party.over(v: 3)
+sleep 1
+party.after
+sleep 2
+party.harder(v: 2)
+sleep 1
+party.ever(v: 1)
+sleep 1
+party.harder(v: 2)
+sleep 1
+party.makesus(v: 2)
+sleep 1
+party.faster(v: 1)
+sleep 1
+party.morethan(v: 2)
+sleep 1
+party.never(v: 1)
+sleep 1
+party.faster(v: 2)
+sleep 1
+party.doit(v: 2)
+sleep 1
+party.faster(v: 2)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.better(v: 2)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.harder(v: 2)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.better(v: 2)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.morethan(v: 1)
+sleep 1
+party.workit(v: 1)
+sleep 1
+party.workit(v: 1)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.workit(v: 1)
+sleep 1
+party.doit(v: 1)
+sleep 1
+party.workit(v: 1)
+sleep 1
+party.better(v: 1)
+sleep 1
+party.workit(v: 2 )
+sleep 1
+party.workit(v: 1)
+sleep 1
+party.workit(v: 2)
+sleep 1
+party.workit(v: 1)
+sleep 1
+
+#binding.pry
 
